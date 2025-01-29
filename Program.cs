763a9cacfa27aba0775bcb6b64ceb9bd726b0330
3mr3bdalgawad <amrelomda125@gmail.com>
2025-01-29 07:55:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Answer_OOP_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
