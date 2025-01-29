02a314cd87aeedd2c6aa4af1a0b09218aed897c0
3mr3bdalgawad <amrelomda125@gmail.com>
2025-01-29 08:10:21 +0200

    Exam

---
+++ AnswerText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Answer_OOP_Exam
+{
+    internal class Answers
+    {
+        public int AnswerId { get; set; }
+        public string AnswerText { get; set; }
+    }
+}
+
